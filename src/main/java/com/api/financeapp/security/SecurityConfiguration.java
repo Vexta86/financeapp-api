53fd6dba8ad749c8ac276c9e84167a3b482b5fc6
Sebastian Rodriguez Suarez <129565458+srodriguezsu@users.noreply.github.com>
2025-08-12 20:14:46 -0500

    Removed null in cors

--- src/main/java/com/api/financeapp/security/SecurityConfiguration.java
+++ src/main/java/com/api/financeapp/security/SecurityConfiguration.java
@@ -35,8 +35,7 @@ public class SecurityConfiguration {
                 List.of(
                         "https://finance.intfinity.co",
                         "https://account.intfinity.co",
-                        "http://localhost:3000",
-                        null
+                        "http://localhost:3000"
                 )
         );
         corsConfiguration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
